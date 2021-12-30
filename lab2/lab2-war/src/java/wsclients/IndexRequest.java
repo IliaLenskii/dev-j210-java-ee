eefd8d6bd0aeb78d8462dcf2b9c9f90958edcff9
Ilia Lenskii <il.lenskii@gmail.com>
2021-12-30 20:31:35 +0300

    Лабораторная работа №2 по курсу «DEV-J200. Java EE. Разработка корпоративных приложений»

---
+++ lab2/lab2-war/src/java/wsclients/IndexRequest.java
@@ -0,0 +1,96 @@
+package wsclients;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//import javax.xml.ws.WebServiceRef;
+//import ru.spbstu.hse.j210.NewWebService;
+
+/**
+ *
+ * @author il.lenskii
+ */
+@WebServlet(name = "IndexRequest", urlPatterns = {"/index-request"})
+@MultipartConfig
+public class IndexRequest extends HttpServlet {
+    
+    //@WebServiceRef
+    //private NewWebService ws;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        
+        boolean isPost = "POST".equals(request.getMethod());
+        String mainPage = request.getContextPath();
+        
+        if(!isPost) {
+            
+            response.sendRedirect(mainPage);
+            return;            
+        }
+
+        String postUser = request.getParameter("user");
+        String postMsg = request.getParameter("msg");
+        
+        System.out.println(">> "+ postUser +" __ "+ postMsg);
+
+        response.sendRedirect( mainPage );
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ lab2/lab2-war/web/index.jsp
@@ -0,0 +1,89 @@
+<%@ page session="true" %>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>It's lab2 from DEV-J210. Java EE</title>
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-size: 14px;
+            font-family: Georgia, 'Times New Roman', Times, serif;
+        }
+        html, body{
+            background-color: #ededed;
+            width: 100%;
+            height: 100%;
+        }
+        .container {
+            width: 400px;
+            height: inherit;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .form-select-button {
+            border: 1px solid #ccc;
+            display: block;
+            padding: 6px;
+            text-align: center;
+        }
+        input[type="text"], button, textarea {
+            display: inline-block;
+            padding: 4px;
+            width: 100%;
+        }
+
+        textarea {
+            height: 50px;
+            resize: none;
+        }
+
+        button[type="submit"] {
+            width: 45%;
+            color: green;
+        }
+        button[type="reset"] {
+            width: 25%;
+        }
+        .tp-pars {
+            margin-top: 14px;
+            margin-bottom: 14px;
+        }
+        .tp-pars label,
+        .tp-pars input {
+            display: inline-block;
+        } .tp-pars input {
+            margin-left: 8px;
+        }
+        .tp-pars label:first-child {
+            margin-right: 24px;
+        }
+        
+        .message-block {
+            margin-top: 40%;
+            margin-bottom: 18px;
+            text-align: center;
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+
+        <div class="message-block"></div>
+
+        <form class="form-select-button" action="index-request" method="post" autocomplete="false" enctype="multipart/form-data">
+            <input type="text" name="user" maxlength="150" placeholder="User is..." value="u1">
+            <br />
+            <br />
+            <input type="text" name="msg" maxlength="150" placeholder="Msg is..." value="asdf aksdfj lskdfjskdj">
+            <br />
+            <br />
+            <button type="submit">Send</button>
+        </form>
+    </div>
+</body>
+</html>
